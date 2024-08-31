9d9f938e908f9610dfe84725a65534b1d9f9525e
Zachary Williams <zacharycw10@gmail.com>
2024-08-31 14:41:48 +0100

    work from other laptop

--- smoothplayermovement.py
+++ smoothplayermovement.py
@@ -1,7 +1,8 @@
 import pygame
+import random 
 pygame.init()
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((800,800))
+screen = pygame.display.set_mode((800,650))
 screen.fill((0,0,0))
 move_up = False
 move_right = False
@@ -9,80 +10,73 @@ move_left = False
 move_down = False
 exit = True
 velocity = 0
-gravity = 0.009
+gravity = 0.02
 colour = (100,100,100)
 px = 100
-py = 100
+py = 600
 walls = []
 access = 0
-COORDS = {
-    0:[50,500,400,40],
-    1:[100,600,400,20],
-    2:[75,200,100,100],
-    3:[],
-    4:[],
-    5:[],
-}
+jump = 1
+
 class Wall:
-    def __init__(self,x,y,w,h):
-        self.x = x
+    def __init__(self,y,w,t,h,r,direction):
         self.y = y
         self.w = w
+        self.t = t
         self.h = h
+        self.direction = direction
+        self.x = random.randint(r,800 - self.t)
         self.hitrect = pygame.Rect(self.x,self.y,self.w,self.h)
+
     def move(self):
         if self.x > 800:
             self.x = 0
-        self.x += 1 
+        elif self.x < -75:
+            self.x = 800
+        self.x += self.direction
         self.hitrect = pygame.Rect(self.x,self.y,self.w,self.h)
 
     def render(self,screen):
         pygame.draw.rect(screen,(colour),self.hitrect)
 
-for i in range(3):
-    x = COORDS[access][0]
-    y = COORDS[access][1]
-    w = COORDS[access][2]
-    h = COORDS[access][3]
-    walls.append(Wall(x,y,w,h))
-    access += 1
+for i in range(6):
+    for i in range(3):
+        y = access
+        w = 75
+        walls.append(Wall(y,w,75,20,0,-1))
+    access += 100
+
+walls.append(Wall(630,800,800,20,0,0))
+walls.append(Wall(0,10,800,650,0,0))
+walls.append(Wall(0,10,10,800,790,0))
+#floor,left wall,right wall
 
 while exit:
-    clock.tick(200)
-    player = pygame.Surface((40, 40))
+    clock.tick(120)
+    player = pygame.Surface((25, 25))
     player.fill((200,200,200))
     screen.fill((0,0,0))
-
-    
     screen.blit(player,(px,py))
-    playerrect = pygame.Rect(px,py,40,40)
+    playerrect = pygame.Rect(px,py,25,25)
 
-    #pygame.draw.rect(screen, (100,100,100), pygame.Rect(50, 50, 40, 40))
-    
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = False
             
         elif event.type == pygame.KEYDOWN:
-
-            if event.key == pygame.K_UP:
+            if event.key == pygame.K_UP and jump:
+                jump = False
                 velocity = -1.5
                 move_up = True
-                move_right = False
-                move_left = False
                 move_down = False
 
             elif event.key == pygame.K_LEFT:
                 move_left = True
-                move_up = False
                 move_right = False
-                move_down = False
 
             elif event.key == pygame.K_RIGHT:
                 move_right = True
-                move_up = False
                 move_left = False
-                move_down = False
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
@@ -105,24 +99,46 @@ while exit:
         move_down = False
     else: 
         move_down = True
+
     for wall in walls:
         hitrect = wall.hitrect
+
         if playerrect.colliderect(hitrect):
-        # Move the player away from the collision while considering the intended direction
-            if move_left and not playerrect.colliderect(hitrect.move(-2, 0)):
-                px += 1
-            elif move_right and not playerrect.colliderect(hitrect.move(2, 0)):
+            
+            jump = True
+            if not playerrect.colliderect(hitrect.move(4, 0)):
                 px -= 1
+        # Move the player away from the collision while considering the intended direction
+            if move_left and not playerrect.colliderect(hitrect.move(-4, 0)):
+                px += 1
+
+            elif move_right and not playerrect.colliderect(hitrect.move(4, 0)):
+                px -= 2
+
             elif move_up and not playerrect.colliderect(hitrect.move(0, -4)):
                 velocity = 0
-                
-            elif move_down and not playerrect.colliderect(hitrect.move(0,4)):
+
+            elif move_down and not playerrect.colliderect(hitrect.move(0, 4)):
                 velocity = 0
+
+            
+            elif move_right and move_down and not playerrect.colliderect(hitrect.move(4, 4)):
+                px -= 2
+                
+            elif move_right and move_up and not playerrect.colliderect(hitrect.move(4, -4)):
+                px -= 2
+                
+            elif move_left and move_down and not playerrect.colliderect(hitrect.move(-4, 4)):
+                px += 2
+                
+            elif move_left and move_up and not playerrect.colliderect(hitrect.move(-4, -4)):
+                px += 2
+                    
             
     py += velocity
     velocity += gravity
-    #for wall in walls:
-        #wall.move()
+
     for wall in walls:
         wall.render(screen)
+        wall.move()
     pygame.display.flip()
